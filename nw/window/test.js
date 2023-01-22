f66f36843cca65ade53e83745b939c98ac219905
tharatau <ayushmxn@outlook.com>
2023-01-22 02:39:56 -0500

    migrate from python to javascript (#6)
    * wip
    
    * fix driver setup
    
    * add width and id
    
    * no sandbox
    
    * try in windows ci
    
    * try in macos ci
    
    * even though ci will fail almost always, it will inform us of platform specific bugs

---
+++ nw/window/test.js
@@ -0,0 +1,51 @@
+import { equal } from "node:assert";
+import { after, before, describe, it } from "node:test";
+import { dirname, relative } from "node:path";
+import { cwd } from "node:process";
+
+import { By } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome.js";
+
+const { Driver, ServiceBuilder, Options } = chrome;
+
+describe("nw window tests", async () => {
+  let driver = undefined;
+
+  before(async () => {
+    const options = new Options();
+    options.addArguments(
+      `nwapp=${relative(
+        cwd(),
+        relative(cwd(), dirname(import.meta.url).slice(7))
+      )}`
+    );
+
+    const service = new ServiceBuilder(
+      `${cwd()}/node_modules/nw/nwjs/chromedriver`
+    ).build();
+
+    driver = Driver.createSession(options, service);
+  });
+
+  it("get window height", async () => {
+    const heightElement = await driver.findElement(By.id("nw-window-height"));
+    const height = await heightElement.getText();
+    equal(height, "100");
+  });
+
+  it("get window width", async () => {
+    const widthElement = await driver.findElement(By.id("nw-window-width"));
+    const width = await widthElement.getText();
+    equal(width, "100");
+  });
+
+  it("get window id", async () => {
+    const idElement = await driver.findElement(By.id("nw-window-id"));
+    const id = await idElement.getText();
+    equal(typeof id, "string");
+  });
+
+  after(() => {
+    driver.quit();
+  });
+});
