84d7716690e4cd4abc6fea05f791451e0e7a24de
Ayushman Chhabra <ayushmxn@outlook.com>
2023-01-22 01:24:24 -0500

    add width and id

--- nw/window/test.js
+++ nw/window/test.js
@@ -3,7 +3,7 @@ import { after, before, describe, it } from "node:test";
 import { dirname, relative } from "node:path";
 import { cwd } from "node:process";
 
-import { Capabilities, Builder, By } from "selenium-webdriver";
+import { By } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome.js";
 
 const { Driver, ServiceBuilder, Options } = chrome;
@@ -13,11 +13,12 @@ describe("NW Window API tests", async () => {
 
   before(async () => {
     const options = new Options();
-    options.addArguments([
-      "nwapp=" +
-        relative(cwd(), relative(cwd(), dirname(import.meta.url).slice(7))),
-    ]);
-    options.setPageLoadStrategy("normal");
+    options.addArguments(
+      `nwapp=${relative(
+        cwd(),
+        relative(cwd(), dirname(import.meta.url).slice(7))
+      )}`
+    );
 
     const service = new ServiceBuilder(
       `${cwd()}/node_modules/nw/nwjs/chromedriver`
@@ -26,10 +27,22 @@ describe("NW Window API tests", async () => {
     driver = Driver.createSession(options, service);
   });
 
-  it("Set Window height", async () => {
+  it("get window height", async () => {
     const heightElement = await driver.findElement(By.id("nw-window-height"));
     const height = await heightElement.getText();
     equal(height, "100");
+  });
+
+  it("get window width", async () => {
+    const widthElement = await driver.findElement(By.id("nw-window-width"));
+    const width = await widthElement.getText();
+    equal(width, "100");
+  });
+
+  it("get window id", async () => {
+    const idElement = await driver.findElement(By.id("nw-window-id"));
+    const id = await idElement.getText();
+    equal(typeof id, "string");
   });
 
   after(() => {
