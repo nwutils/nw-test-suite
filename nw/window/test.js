e07426ebb3a3172f65b83e5ad755aef99c670c77
Ayushman Chhabra <ayushmxn@outlook.com>
2023-01-22 01:04:27 -0500

    fix driver setup

--- nw/window/test.js
+++ nw/window/test.js
@@ -1,30 +1,38 @@
 import { equal } from "node:assert";
 import { after, before, describe, it } from "node:test";
+import { dirname, relative } from "node:path";
 import { cwd } from "node:process";
 
 import { Capabilities, Builder, By } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome.js";
 
-const { ServiceBuilder, setDefaultService } = chrome;
+const { Driver, ServiceBuilder, Options } = chrome;
 
 describe("NW Window API tests", async () => {
-  let driver = null;
+  let driver = undefined;
 
   before(async () => {
-    const service = new ServiceBuilder(`${cwd()}/node_modules/nw/nwjs/chromedriver`).build();
-    setDefaultService(service);
-    driver = await new Builder()
-      .withCapabilities(Capabilities.chrome())
-      .build();
+    const options = new Options();
+    options.addArguments([
+      "nwapp=" +
+        relative(cwd(), relative(cwd(), dirname(import.meta.url).slice(7))),
+    ]);
+    options.setPageLoadStrategy("normal");
+
+    const service = new ServiceBuilder(
+      `${cwd()}/node_modules/nw/nwjs/chromedriver`
+    ).build();
+
+    driver = Driver.createSession(options, service);
   });
 
   it("Set Window height", async () => {
-    let heightElement = await driver.findElement(By.id("window-height"));
-    let height = await heightElement.getText();
+    const heightElement = await driver.findElement(By.id("nw-window-height"));
+    const height = await heightElement.getText();
     equal(height, "100");
   });
 
-  after(async () => {
-    await driver.quit();
+  after(() => {
+    driver.quit();
   });
 });
