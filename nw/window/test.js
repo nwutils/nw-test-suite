4a564858824cda545080676fe1544b133f7f5520
Ayushman Chhabra <ayushmxn@outlook.com>
2023-01-29 13:45:30 -0500

    use `/tmp` instead of `/dev/shm`

--- nw/window/test.js
+++ nw/window/test.js
@@ -1,7 +1,7 @@
 import { equal } from "node:assert";
 import { after, before, describe, it } from "node:test";
 import { dirname, relative } from "node:path";
-import { cwd } from "node:process";
+import { cwd, platform } from "node:process";
 
 import { By } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome.js";
@@ -13,12 +13,16 @@ describe("nw window tests", async () => {
 
   before(async () => {
     const options = new Options();
-    options.addArguments(
+    const args = [
       `nwapp=${relative(
         cwd(),
         relative(cwd(), dirname(import.meta.url).slice(7))
-      )}`
-    );
+      )}`,
+    ]
+    if (platform === "linux") {
+      args.push("--disable-dev-shm-usage");
+    }
+    options.addArguments(args);
 
     const service = new ServiceBuilder(
       `${cwd()}/node_modules/nw/nwjs/chromedriver`
