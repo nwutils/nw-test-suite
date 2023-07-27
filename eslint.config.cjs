563cd9fe7a0f3c3ce50d653671cac11570a56070
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-07-27 12:34:12 +0530

    ci: add format and linting

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,17 +1,11 @@
 module.exports = {
     parserOptions: {
-      ecmaVersion: 2022,
+      ecmaVersion: 2023,
       sourceType: "module",
     },
     env: {
       es6: true,
       node: true,
     },
-    extends: ["eslint:recommended", "tjw-jsdoc"],
-    rules: {
-      "jsdoc/require-file-overview": "off",
-      "jsdoc/require-returns-description": "off",
-      "jsdoc/match-description": "off",
-      "jsdoc/valid-types": "off",
-    },
+    extends: ["eslint:recommended"],
   };
